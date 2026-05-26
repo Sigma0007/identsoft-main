e96dd24b16c7c64dab7c0c7d38d673303a4b42d5
iDentSoft Developer <developer@identsoft.com>
2026-05-26 10:55:21 +0530

    Fix unterminated comment in routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ use Illuminate\Support\Facades\Schema;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+*/
+
 Route::get('/migrate', function() {
     try {
         \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
